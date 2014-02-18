8bab464cb9a2d513b2491a145506a145d517d496
Cameron Cundiff <pair+ccundiff@pivotallabs.com>
2014-02-18 15:35:59 -0500

    Use default application layout for styleguide

--- app/controllers/hologram_rails/styleguide_controller.rb
+++ app/controllers/hologram_rails/styleguide_controller.rb
@@ -3,6 +3,5 @@ require_dependency "hologram_rails/application_controller"
 module HologramRails
   class StyleguideController < ApplicationController
     include HighVoltage::StaticPage
-    layout "hologram_rails"
   end
 end
